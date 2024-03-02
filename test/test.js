e099979c15fdfd96b3549a4264c3943656e79163
Lisa Donohoo <38710969+ldonohoo@users.noreply.github.com>
2024-03-01 20:01:15 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,1 +1,25 @@
 
+// Trying to validate the bird 
+
+// pulled in the assert module?? maybe from node.js?
+let assert = require('assert');
+
+// pulled in birdObject module so I can test the variables
+let birdObject = require('../scripts/boring-page.js');
+
+
+// describe the test group/group hierarchy
+// and then it which contains the tests for that group
+describe('Validate bird', function () {
+  describe('Bird is assigned to proper value', function () {
+    it('Check if value assigned corresponds to correct bird', function () {
+        if (birdObject.birdRandomizer === 0) {
+            assert.match(birdObject.selectedBird, /osprey/);
+        } else if (birdObject.birdRandomizer === 1) {
+            assert.match(birdObject.selectedBird, /raven/);
+        } else if (birdObject.birdRandomizer === 2) {
+            assert.match(birdObject.selectedBird, /goose/);
+        }
+    });
+  });
+});
